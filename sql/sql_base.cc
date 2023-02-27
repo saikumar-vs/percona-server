b5479244669edb48943fcc17ca2e4e1ba8efb271
saikumar-vs <saikumar.vs@percona.com>
2023-02-27 17:15:25 +0530

    Gcov second commit

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -20,6 +20,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
+
 /* Basic functions needed by many modules */
 
 #include "sql/sql_base.h"
